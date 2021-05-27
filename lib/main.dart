d08567d4ecc579740e0f86512f7f5efc07b88ab0
Arthur Thompson <arthurthompson@google.com>
2021-05-26 18:35:27 -0700

    feat: initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
@@ -22,56 +23,79 @@ void main() {
   runApp(const GalleryApp());
 }
 
-class GalleryApp extends StatelessWidget {
-  const GalleryApp({
-    Key key,
-    this.initialRoute,
-    this.isTestMode = false,
-  }) : super(key: key);
+class GalleryApp extends StatefulWidget {
+  const GalleryApp({Key key, this.initialRoute, this.isTestMode = false}) : super(key: key);
 
   final bool isTestMode;
   final String initialRoute;
 
   @override
+  _GalleryAppState createState() => _GalleryAppState();
+}
+
+class _GalleryAppState extends State<GalleryApp> {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
   Widget build(BuildContext context) {
-    return ModelBinding(
-      initialModel: GalleryOptions(
-        themeMode: ThemeMode.system,
-        textScaleFactor: systemTextScaleFactorOption,
-        customTextDirection: CustomTextDirection.localeBased,
-        locale: null,
-        timeDilation: timeDilation,
-        platform: defaultTargetPlatform,
-        isTestMode: isTestMode,
-      ),
-      child: Builder(
-        builder: (context) {
-          return MaterialApp(
-            restorationScopeId: 'rootGallery',
-            title: 'Flutter Gallery',
-            debugShowCheckedModeBanner: false,
-            themeMode: GalleryOptions.of(context).themeMode,
-            theme: GalleryThemeData.lightThemeData.copyWith(
-              platform: GalleryOptions.of(context).platform,
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print('something went wrong');
+          return Container();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ModelBinding(
+            initialModel: GalleryOptions(
+              themeMode: ThemeMode.system,
+              textScaleFactor: systemTextScaleFactorOption,
+              customTextDirection: CustomTextDirection.localeBased,
+              locale: null,
+              timeDilation: timeDilation,
+              platform: defaultTargetPlatform,
+              isTestMode: widget.isTestMode,
             ),
-            darkTheme: GalleryThemeData.darkThemeData.copyWith(
-              platform: GalleryOptions.of(context).platform,
+            child: Builder(
+              builder: (context) {
+                return MaterialApp(
+                  restorationScopeId: 'rootGallery',
+                  title: 'Flutter Gallery',
+                  debugShowCheckedModeBanner: false,
+                  themeMode: GalleryOptions.of(context).themeMode,
+                  theme: GalleryThemeData.lightThemeData.copyWith(
+                    platform: GalleryOptions.of(context).platform,
+                  ),
+                  darkTheme: GalleryThemeData.darkThemeData.copyWith(
+                    platform: GalleryOptions.of(context).platform,
+                  ),
+                  localizationsDelegates: const [
+                    ...GalleryLocalizations.localizationsDelegates,
+                    LocaleNamesLocalizationsDelegate()
+                  ],
+                  initialRoute: widget.initialRoute,
+                  supportedLocales: GalleryLocalizations.supportedLocales,
+                  locale: GalleryOptions.of(context).locale,
+                  localeResolutionCallback: (locale, supportedLocales) {
+                    deviceLocale = locale;
+                    return locale;
+                  },
+                  onGenerateRoute: RouteConfiguration.onGenerateRoute,
+                );
+              },
             ),
-            localizationsDelegates: const [
-              ...GalleryLocalizations.localizationsDelegates,
-              LocaleNamesLocalizationsDelegate()
-            ],
-            initialRoute: initialRoute,
-            supportedLocales: GalleryLocalizations.supportedLocales,
-            locale: GalleryOptions.of(context).locale,
-            localeResolutionCallback: (locale, supportedLocales) {
-              deviceLocale = locale;
-              return locale;
-            },
-            onGenerateRoute: RouteConfiguration.onGenerateRoute,
           );
-        },
-      ),
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        print('loading...');
+        return Container(child: const Center(child: Text('loading...', textDirection: TextDirection.ltr,)),);
+      },
     );
   }
 }
